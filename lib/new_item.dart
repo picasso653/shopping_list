ec1041db885c50df0fd5e326c90eb7642ba82af1
Isaac Korankye Asante <xquisit31@gmail.com>
2024-01-07 21:35:20 +0000

    setting validators

--- lib/new_item.dart
+++ lib/new_item.dart
@@ -26,7 +26,13 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  return 'Demo';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length == 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters long.';
+                  }
+                  return null;
                 },
               ),
               Row(
@@ -38,6 +44,15 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      int.tryParse(value) == null ||
+                      int.tryParse(value)! <= 0) {
+                    return 'Must be between 1 and 50 characters long.';
+                  }
+                  return null;
+                },
                     ),
                   ),
                   const SizedBox(
@@ -68,7 +83,9 @@ class _NewItemState extends State<NewItem> {
                   ),
                 ],
               ),
-              const SizedBox(height: 12,),
+              const SizedBox(
+                height: 12,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
